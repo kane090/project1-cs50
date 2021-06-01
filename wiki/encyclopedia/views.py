1833aa4ecd1bb2237e2c2e00a201e6e22535181a
Rishith Nellippillil <rishu.ks@gmail.com>
2021-06-01 22:53:37 +0300

    Fulfilled: Entry Page, Index Page, Search, Markdown to HTML Conversion

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,10 +1,37 @@
 from django.shortcuts import render
+from markdown2 import markdown
 
 from . import util
 
 
 def index(request):
-    return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
-    })
+    page_name = request.GET.get('q')
+    if page_name != None:
+        return search(request, page_name)
+    else:
+        return render(request, "encyclopedia/index.html", {
+            "entries": util.list_entries()
+        })
 
+def entry(request, page_name):
+    try:
+        return render(request, "encyclopedia/entry.html", {
+            "title": page_name.capitalize(),
+            "entry": markdown(util.get_entry(page_name))
+    })
+    except TypeError:
+        return render(request, "encyclopedia/error.html")
+
+def search(request, page_name):
+    subset_entries = []
+    og_entries_list = util.list_entries()
+    entries_list = [entry.lower() for entry in og_entries_list]
+    if page_name in entries_list:
+        return entry(request, page_name)
+    else:
+        for i in entries_list:
+            if page_name in i:
+                subset_entries.append(i)
+        return render(request, "encyclopedia/search_results.html", {
+            "entries": subset_entries
+        })
